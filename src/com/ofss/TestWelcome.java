99509b2a9ff8135f21c942dc91b661b26a62f256
Guru <gurumurthy.ramamurthy@gmail.com>
2019-11-15 16:18:06 +0530

    pushing to github to verify the build trigger

--- src/com/ofss/TestWelcome.java
+++ src/com/ofss/TestWelcome.java
@@ -5,6 +5,7 @@ public class TestWelcome {
 	public static void main(String[] args) {
 		System.out.println("Test Welcome in github");
 		System.out.println("2nd line");
+		System.out.println("Built using github-jenkins pipeline");
 
 	}
 
